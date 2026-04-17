768d31968979e46e8410caa1d1273ea7e8545264
yeojincho <yeojincho0129@gmail.com>
2026-04-17 16:23:55 +0900

    feat: 260417 수업내용

--- api/main.py
+++ api/main.py
@@ -1,13 +1,46 @@
-from fastapi import FastAPI
-from sqlalchemy import text
-from connection import SessionFactory
+import uuid
+import json
+
+from fastapi import FastAPI, Body
+from fastapi.responses import StreamingResponse
+from redis import asyncio as aredis
+
+redis_client = aredis.from_url("redis://redis:6379", decode_responses=True) # redis는 도커에서 서비스 이름으로 접근 가능
 
 app = FastAPI()
 
-@app.get("/health-check")
-def health_check_handler():
-    # 컨텍스트 매니지 문법
-    with SessionFactory() as session:
-        stm = text("SELECT * FROM user LIMIT 1;")
-        row = session.execute(stm).fetchone() # Row 객체 반환
-    return {"user": row._asdict()}
+@app.post("/generate")
+async def generate_chat_handler(
+    user_input : str = Body(..., embed=True),
+):
+    # 1) 요청 본문(user_input) 
+    # 2) 채널 구독 
+    channel = str(uuid.uuid4())
+    pubsub = redis_client.pubsub()
+    await pubsub.subscribe(channel)
+    
+    # 3) Queue를 통해 Worker에게 task를 전달(enqueue)
+    task = {"channel": channel, "user_input": user_input}
+    await redis_client.lpush("queue", json.dumps(task))  # dict 형태의 task를 텍스트로 바꿈
+
+    # 4) 채널 메시지 읽고, 토큰 반환
+    async def event_generator():
+        async for message in pubsub.listen():
+            if message["type"] != "message":
+                continue
+            
+            token = message["data"]
+            if token == "[DONE]":
+                break
+            yield token
+            
+        await pubsub.unsubscribe(channel)
+        await pubsub.close()
+
+    # 5) 결과수신
+    return StreamingResponse(
+        event_generator(),
+        media_type="text/event-stream",
+    )
+    
+    return
--- worker/main.py
+++ worker/main.py
@@ -1,4 +1,8 @@
+import json
+import redis
 from llama_cpp import Llama
+
+redis_clienet = redis.from_url("redis://redis:6379", decode_responses=True)
 
 
 llm = Llama(
@@ -15,14 +19,38 @@ SYSTEM_PROMPT = (
     "Do not change the language. "
     "Do not mix languages."
 )
-user_input = "도커가 뭐야?"
-response = llm.create_chat_completion(
-    messages=[
-        {"role": "system", "content": SYSTEM_PROMPT},
-        {"role": "user", "content": user_input},
-    ],
-    max_tokens=256,
-    temperature=0.7,
-)
 
-print(response["choices"][0]["message"]["content"])
+def run():
+    while True: # 무한루프로 하지 않으면 작업 하나 끝나면 종료되어버림
+
+        # 1) 큐에서 task를 dequeue
+        _, task = redis_clienet.brpop("queue") # rpop:아무것도 없으면 nil 반환, block rpop: 아무것도 없으면 들어올때까지 대기
+        
+        task_data : dict = json.loads(task)
+
+
+        # 2) 추론 -> 토큰 -> publish (반복)
+        response_generator = llm.create_chat_completion(
+            messages=[
+                {"role": "system", "content": SYSTEM_PROMPT},
+                {"role": "user", "content": task_data["user_input"]},
+            ],
+            max_tokens=256,
+            temperature=0.7,
+            stream=True, # 이게 없으면 한번에 오는건데, stream=True이면 제너레이터를 먼저 만든다. 
+        )
+        # 즉, 여기까지는 아직 추론을 하지 않은 상태임
+
+        channel = task_data["channel"]
+        for chunk in response_generator:
+            token = chunk["choices"][0]["delta"].get("content")
+             
+            if token:
+                redis_clienet.publish(channel, token)
+
+        # 3) 추론이 종료되었음을 알림: [Done] 메시지 전송
+        redis_clienet.publish(channel, "[DONE]")
+
+# 이 파일이 직접 실행한 경우에만, run() 호출
+if __name__ == "__main__":
+    run()
